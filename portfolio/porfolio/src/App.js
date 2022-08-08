e91eb1450b9b4e7308adc086fd6ff370758138a7
hassen avila <hassenavilaquintero@gmail.com>
2022-08-08 00:24:51 -0300

    home

--- portfolio/porfolio/src/App.js
+++ portfolio/porfolio/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Home from './frontend/home/home';
+import Pages from './frontend/pages/pages';
 
 function App() {
   return (
     <>
     <Home/>
+   
     </>
   );
 }
